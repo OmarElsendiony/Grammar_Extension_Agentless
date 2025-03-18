c6afa5fe4dd48634927328fb9a9d05bbd338e375
Omar Elsendiony <omar.sendiony@gmail.com>
2025-03-18 17:47:37 +0200

    NFR: Added movable grammar window functionality

--- scripts/script.js
+++ scripts/script.js
@@ -165,6 +165,54 @@ function grammar_checker_logic(logoURL) {
         const range = editor.getModel().getFullModelRange(); // magic
         editor.executeEdits("", [{ range, text, forceMoveMarkers: true }]);
     });
+
+    dragElement(document.getElementById("form_grammar"));
+
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        
+        // Get the header element to be the drag handle
+        elmnt.onmousedown = dragMouseDown;
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            const target = e.target || e.srcElement;
+            if (target.tagName === 'INPUT' || 
+                target.tagName === 'TEXTAREA' || 
+                target.tagName === 'BUTTON' || 
+                target.tagName === 'SELECT') {
+                return;
+            }
+
+            e.preventDefault();
+            // Get the mouse cursor position at startup
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // Call a function whenever the cursor moves
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // Calculate the new cursor position
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // Set the element's new position
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // Stop moving when mouse button is released
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
     editorWindow.focus();
 }
 
